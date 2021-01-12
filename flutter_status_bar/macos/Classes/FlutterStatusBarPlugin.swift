c8b364f57baa1c1a6094dccf7a51b9f54ea0d57f
CoolSnow <670696652@qq.com>
2021-01-12 18:31:23 +0800

    project init

---
+++ flutter_status_bar/macos/Classes/FlutterStatusBarPlugin.swift
@@ -0,0 +1,19 @@
+import Cocoa
+import FlutterMacOS
+
+public class FlutterStatusBarPlugin: NSObject, FlutterPlugin {
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "flutter_status_bar", binaryMessenger: registrar.messenger)
+    let instance = FlutterStatusBarPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch call.method {
+    case "getPlatformVersion":
+      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
+    default:
+      result(FlutterMethodNotImplemented)
+    }
+  }
+}
