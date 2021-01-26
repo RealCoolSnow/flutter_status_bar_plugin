82b05121c28cab6bc3517a865e68fb3d30bae703
CoolSnow <670696652@qq.com>
2021-01-26 22:21:45 +0800

    add windows

---
+++ flutter_status_bar/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_status_bar/flutter_status_bar_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterStatusBarPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterStatusBarPlugin"));
+}
